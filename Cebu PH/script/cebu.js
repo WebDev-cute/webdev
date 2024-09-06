80f5f2527d922fb14e767adfd1dd8556ef368ba2
ArchielDev's <cabanagarchiel@gmail.com>
2024-09-06 19:19:34 +0800

    Add files via upload

---
+++ Cebu PH/script/cebu.js
@@ -0,0 +1,16 @@
+
+// SHOW SIDEBAR HERE
+function showsidebar () {
+
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebar.style.display='flex'
+}
+
+// HIDE SIDEBAR HERE
+function hidesidebar () {
+
+    const sidebar = document.querySelector('.sidebar');
+
+    sidebar.style.display='none'
+}
